20ac1e02fd06c46de10d422bb57ce38058215695
mopdzz <mopdzz@163.com>
2015-12-03 22:47:08 +0800

    骏网9元改为10元

--- com.gamecard.platform.dao/src/main/java/com/sp/platform/dao/PriceDao.java
+++ com.gamecard.platform.dao/src/main/java/com/sp/platform/dao/PriceDao.java
@@ -2,6 +2,7 @@ package com.sp.platform.dao;
 
 import com.sp.platform.entity.Price;
 import com.yangl.common.hibernate.HibernateDaoUtil;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import org.springframework.stereotype.Repository;
 
@@ -16,8 +17,16 @@ import java.util.List;
  */
 @Repository
 public class PriceDao extends HibernateDaoUtil<Price, Integer> {
-    public List<Price> getPriceByCardId(int cardId){
-        return getSessionFactory().getCurrentSession().createSQLQuery("select a.* from gc_price a, gc_card_price b " +
+    public List<Price> getPriceByCardId(int cardId) {
+        List<Price> prices = getSessionFactory().getCurrentSession().createSQLQuery("select a.* from gc_price a, gc_card_price b " +
                 "where a.id = b.priceId and b.cardId = " + cardId + " order by a.price").addEntity(Price.class).list();
+        if (cardId == 1 && CollectionUtils.isNotEmpty(prices)) {
+            for (Price price : prices) {
+                if (16 == price.getId()) {
+                    price.setDescription("10元卡");
+                }
+            }
+        }
+        return prices;
     }
 }
